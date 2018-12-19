f66a8f3201f6f2466b89887a4ec31d695d57cb24
Cliff Parnitzky <github@cliff-parnitzky.de>
2018-12-19 21:50:30 +0100

    Use shorter namespace

--- system/modules/BackendMultiEditAssistant/classes/BackendMultiEditAssistant.php
+++ system/modules/BackendMultiEditAssistant/classes/BackendMultiEditAssistant.php
@@ -30,7 +30,7 @@
 /**
  * Run in a custom namespace, so the class can be replaced
  */
-namespace CliffParnitzky\BackendMultiEditAssistant; 
+namespace CliffParnitzky; 
 
 /**
 * Class BackendMultiEditAssistant
